f076b2c0f74190ad0da7b5015b04eb3ef76c2ccc
Ben Belchak <ben@needle.com>
2012-08-09 16:55:18 -0600

    Better output formatting for events in hipchat

--- handlers/notification/hipchat.rb
+++ handlers/notification/hipchat.rb
@@ -14,9 +14,9 @@ class HipChatNotif < Sensu::Handler
   def handle
     hipchatmsg = HipChat::Client.new(settings["hipchat"]["apikey"])
     if @event['action'].eql?("resolve")
-      hipchatmsg[settings["hipchat"]["room"]].send('Sensu', "RESOLVED - [#{event_name}] - #{@event['check']['notification']}.", :color => 'green')
+      hipchatmsg[settings["hipchat"]["room"]].send('Sensu', "RESOLVED - [#{event_name}] - #{@event['check']['notification']} <br/> OUTPUT: #{@event['check']['output']}.", :color => 'green')
     else
-      hipchatmsg[settings["hipchat"]["room"]].send('Sensu', "ALERT - [#{event_name}] - #{@event['check']['notification']}.", :color => 'red', :notify => true)
+      hipchatmsg[settings["hipchat"]["room"]].send('Sensu', "ALERT - [#{event_name}] - #{@event['check']['notification']} <br/> OUTPUT: #{@event['check']['output']}.", :color => 'red', :notify => true)
     end
   end
 
