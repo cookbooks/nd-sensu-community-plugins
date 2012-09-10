ef0bc0246e98d3fb08f9035ba4aae92c3d7faf37
Ben Belchak <ben@needle.com>
2012-09-10 16:19:05 -0600

    adding partners to hipchat notification

--- handlers/notification/hipchat.rb
+++ handlers/notification/hipchat.rb
@@ -17,10 +17,13 @@ class HipChatNotif < Sensu::Handler
 
   def handle
     hipchatmsg = HipChat::Client.new(settings["hipchat"]["apikey"])
+    partners_str = ""
+    if @event['client'].has_key?('partners')
+      partners_str = "<br/>PARTNERS: #{@event['client']['partners'].join(', ')}"
     if @event['action'].eql?("resolve")
-      hipchatmsg[settings["hipchat"]["room"]].send('Sensu', "RESOLVED - [#{event_name}] - #{@event['check']['notification']} <br/> OUTPUT: #{@event['check']['output']} <br/> SUBS: #{@event['client']['subscriptions']}", :color => 'green')
+      hipchatmsg[settings["hipchat"]["room"]].send('Sensu', "RESOLVED - [#{event_name}] - #{@event['check']['notification']} <br/> OUTPUT: #{@event['check']['output']} <br/> SUBS: #{@event['client']['subscriptions']} #{partners_str}", :color => 'green')
     else
-      hipchatmsg[settings["hipchat"]["room"]].send('Sensu', "ALERT - [#{event_name}] - #{@event['check']['notification']} <br/> OUTPUT: #{@event['check']['output']} <br/> SUBS: #{@event['client']['subscriptions']}", :color => 'red', :notify => true)
+      hipchatmsg[settings["hipchat"]["room"]].send('Sensu', "ALERT - [#{event_name}] - #{@event['check']['notification']} <br/> OUTPUT: #{@event['check']['output']} <br/> SUBS: #{@event['client']['subscriptions']} #{partners_str}", :color => 'red', :notify => true)
     end
   end
 
